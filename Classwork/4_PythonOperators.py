bc5beb319d2c613d4fc22a94ca63d4e0be2ea82f
Anjanikrupal <anjanikrupal@gmail.com>
2025-07-09 09:34:03 +0530

    frist

---
+++ Classwork/4_PythonOperators.py
@@ -0,0 +1,105 @@
+#1. Arthimetic Operators
+a = 10
+b = 20
+print("Addition:",a+b) #Addition: 30
+print("Subtraction:",a-b) #Subtraction: -10
+print("Multiplication:",a*b) #Multiplication: 200
+print("Division:",a/b) #Division: 0.5
+print("FloorDivision:",a//b) #FloorDivision: 0
+print("Modulus:",a%b) #Modulus: 10
+print("Exponential:",a**b) #Exponential: 100000000000000000000
+
+
+
+#2. Comparision Operators
+c = 20
+d = 40
+print(c==d) #Equalto(==): False
+
+print(c!=d) #Noteuqalto(!=): True
+
+print(c>d) #Greaterthan(>): False
+
+print(c<d) #Lessthan(<): True
+
+print(c>=d) #GreaterthanorEqualto(>=): False
+
+print(c<=d) #LessthanorEqualto(<=): True
+
+
+#3.Assignment Operators
+x = 10
+print("Assign:",x) #Assign: 10
+
+x+=30
+print(x) #Add & Assign(+=): 40
+
+x-=20
+print(x) #Subtract & Assign(-=): 20
+
+x*=2
+print(x) #Multiply & Assign(*=): 40
+
+x/=2
+print(x) #Divide & Assign(/=): 20.0
+
+x//=10
+print(x) #Floor Divide & Assign(//=): 2.0
+
+x%=2
+print(x) #Modulus & Assign(%=): 0.0
+
+x**=10
+print(x) #Exponentiate & Assign(**=): 0.0
+
+
+#4. Logical Operators
+
+print("Logical operators")
+x=30
+y=10
+print("and:",(x%10==0)and (y%10==0)) #and: True
+print("OR:",(x%10==0)or (y%10==0)) #OR: True
+print("not:",not (x%10==0)) #not: False
+
+
+#5.Membership operators
+
+Names = ['suman','ashok','manoj','arjun']
+print('in result:','rahul' in Names)  # in result: False
+print('not in result:','suhas' not in Names) # not in result: True
+
+
+#6. Identity operator
+
+l = [1,2,3,4]
+b = [1,2,3,4]
+print("l is b:",l is b) #l is b: False
+
+a = b
+print("a is b:",a is b) #a is b: True
+
+print("id(l):",id(l)) #id(l): 2330410329280
+
+print("id(b):",id(b)) #id(b): 2330412008640
+
+print("id(a):",id(a)) #id(a): 2330412008640
+
+print("a is not b:",a is not b) #a is not b: False
+
+print("l is not b:",l is not b) #l is not b: True
+
+
+#7.Bitwise Operators
+
+print("Bitwise Operators:")
+print("and operator 3 & 5:",3&5)
+print("or operator  4 | 5:",4|5)
+print("xor operator 5 ^ 6:",5^6)
+print("~1 not operator : ",~1)
+print("left shift operator by   1 bit 4<<1: ",2<<1)#Multiply by 2
+print("left shift operator by   2 bit 4<<2: ",4<<2)
+print("right shift operator by  1 bit 4>>1: ",4>>1)# Divide by 2
+print("right shift operator by  2 bit 4>>2: ",4>>2)
+
+
---
+++ Classwork/5_Strings.py
@@ -0,0 +1,56 @@
+name = "sanjay"
+print(name)
+
+print(name.ljust(30,' '))
+
+print(name.ljust(30,' ')+":") #leftside
+
+print(name.rjust(30," ")+":")  #rightside
+
+print('56'.zfill(6))  #000056
+
+print(''.zfill(10))  #0000000000
+
+w = 'arjun is a cop'
+print(w)            #arjun is a cop
+
+print(w.find('a'))  #0
+
+print(w.rfind('a')) #9
+
+print(w.index('a'))
+#0
+
+print(w.rindex('a'))
+#9
+
+print(w.find('z'))
+#-1
+
+#name.index('z')    #error when it can't find
+
+print(w.count('a'))  #counts
+#2
+
+print(w.startswith("arjun")) #True
+
+d = 'abc'.isalpha()  #check it is alphabet or not
+#True
+d = 'abc123'.isalpha()
+#False
+di = '123'.isdigit()  #checks digits or not
+True
+n = '1.23'.isdigit()
+False
+n = 'arjun123'.isalnum()  #checks numerics and alphabets and no other special chars
+'arjun@123'.isalnum()
+False
+'arjun'.islower()
+True
+'Arjun'.islower()
+False
+'ARJUN'.isupper()
+True
+
+s = ' '.isspace()  #checks whether having space
+True
